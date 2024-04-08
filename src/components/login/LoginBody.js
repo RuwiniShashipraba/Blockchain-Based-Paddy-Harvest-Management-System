a738bc4a4467a79b4d17b40132506c50c9ce9178
Vishvi De Silva <91365901+Vishvi98@users.noreply.github.com>
2024-04-08 13:38:41 +0530

    change styles

--- src/components/login/LoginBody.js
+++ src/components/login/LoginBody.js
@@ -3,18 +3,25 @@ import LoginLeftCard from "./LoginLeftCard";
 
 import "../../styles/Login.css";
 import LoginRightCard from "./LoginRightCard";
+import NavigationBar from "../FrontPage/NavigationBar";
+import { Container } from "@material-ui/core";
+import Footer from "../FrontPage/Footer";
 
 function LoginBody() {
-    return (
-        <div className="flex even-steven">
-            <div className="w-60">
-                <LoginLeftCard/>
-            </div>
-            <div className="w-30 self-center">
-                <LoginRightCard/>
-            </div>
+  return (
+    <Container>
+      <NavigationBar />
+      <div className="flex even-steven">
+        <div className="w-60">
+          <LoginLeftCard />
         </div>
-    );
+        <div className="w-30 self-center">
+          <LoginRightCard />
+        </div>
+      </div>
+      <Footer />
+    </Container>
+  );
 }
 
 export default LoginBody;
--- src/components/sign/SignUp.js
+++ src/components/sign/SignUp.js
@@ -5,6 +5,8 @@ import { eye } from "react-icons-kit/feather/eye";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import "../../styles/sign.css";
+import Footer from "../FrontPage/Footer";
+import NavigationBar from "../FrontPage/NavigationBar";
 
 const SignUp = () => {
   const [fullName, setFullName] = useState("");
@@ -59,176 +61,180 @@ const SignUp = () => {
   };
 
   return (
-    <div className="container">
-      <div className="image">
-        <img />
-      </div>
-      <div className="form-container">
-        {/* <div className="form-header">
+    <div>
+      <NavigationBar/>
+      <div className="container">
+        <div className="image">
+          <img />
+        </div>
+        <div className="form-container">
+          {/* <div className="form-header">
           <h2>Let's Create an Account</h2>
         </div> */}
-        <form>
-          <div className="formdetails">
-            <div className="form-group">
-              <div className="form-header">
-                <h2>Let's Create an Account</h2>
+          <form>
+            <div className="formdetails">
+              <div className="form-group">
+                <div className="form-header">
+                  <h2>Let's Create an Account</h2>
+                </div>
+                <input
+                  type="text"
+                  className="input"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                  required
+                  placeholder="Enter your full name"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="input"
+                  value={uaddress}
+                  onChange={(e) => setuAddress(e.target.value)}
+                  required
+                  placeholder="Address"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="input"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                  placeholder="Email"
+                />
+                {emailError && <div className="error">{emailError}</div>}
+              </div>
+              <div className="form-group" style={{ position: "relative" }}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="input"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  placeholder="Password"
+                />
+                {password.length > 0 && password.length < 4 && (
+                  <div className="error">
+                    Password must be at least 4 characters
+                  </div>
+                )}
+                <span
+                  className="toggle-password"
+                  onClick={() => setShowPassword(!showPassword)}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "10px",
+                    transform: "translateY(-50%)",
+                  }} // Toggle the showPassword state
+                >
+                  {showPassword ? <Icon icon={eye} /> : <Icon icon={eyeOff} />}
+                </span>
+              </div>
+              <div className="form-group" style={{ position: "relative" }}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="input"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  placeholder="Confirm Password"
+                />
+                {confirmPassword !== password && (
+                  <div className="error">Passwords do not match</div>
+                )}
+                <span
+                  className="toggle-password"
+                  onClick={() => setShowPassword(!showPassword)}
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "10px",
+                    transform: "translateY(-50%)",
+                  }} // Toggle the showPassword state
+                >
+                  {showPassword ? <Icon icon={eye} /> : <Icon icon={eyeOff} />}
+                </span>
+              </div>
+              <legend>Are you a</legend>
+              <div className="position">
+                <label>
+                  <input
+                    type="radio"
+                    name="topping"
+                    value="farmer"
+                    onChange={() => setSelectedRole("farmer")}
+                    checked={selectedRole === "farmer"}
+                  />
+                  <span>Farmer</span>
+                </label>
+              </div>
+              <div className="position">
+                <label>
+                  <input
+                    type="radio"
+                    name="topping"
+                    value="miller"
+                    onChange={() => setSelectedRole("miller")}
+                    checked={selectedRole === "miller"}
+                  />
+                  <span>Miller</span>
+                </label>
+              </div>
+              <div className="position">
+                <label>
+                  <input
+                    type="radio"
+                    name="topping"
+                    value="seller"
+                    onChange={() => setSelectedRole("seller")}
+                    checked={selectedRole === "seller"}
+                  />
+                  <span>Seller</span>
+                </label>
+              </div>
+              <div className="position">
+                <label>
+                  <input
+                    type="radio"
+                    name="topping"
+                    value="Customer"
+                    onChange={() => setSelectedRole("customer")}
+                    checked={selectedRole === "customer"}
+                  />
+                  <span>Customer</span>
+                </label>
               </div>
               <input
-                type="text"
-                className="input"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                required
-                placeholder="Enter your full name"
-              />
+                type="checkbox"
+                checked={check}
+                onChange={() => setCheck(!check)}
+              />{" "}
+              I accept the terms of the offer of privacy policy.
+              <div className="sub but">
+                <Button
+                  variant="contained"
+                  onClick={navigateToNext}
+                  disabled={!isFormValid()}
+                >
+                  {" "}
+                  SIGN UP
+                </Button>
+              </div>
             </div>
-            <div className="form-group">
-              <input
-                type="text"
-                className="input"
-                value={uaddress}
-                onChange={(e) => setuAddress(e.target.value)}
-                required
-                placeholder="Address"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                className="input"
-                value={email}
-                onChange={handleEmailChange}
-                required
-                placeholder="Email"
-              />
-              {emailError && <div className="error">{emailError}</div>}
-            </div>
-            <div className="form-group" style={{ position: "relative" }}>
-              <input
-                type={showPassword ? "text" : "password"}
-                className="input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                placeholder="Password"
-              />
-              {password.length > 0 && password.length < 4 && (
-                <div className="error">
-                  Password must be at least 4 characters
-                </div>
-              )}
-              <span
-                className="toggle-password"
-                onClick={() => setShowPassword(!showPassword)}
-                style={{
-                  position: "absolute",
-                  top: "50%",
-                  right: "10px",
-                  transform: "translateY(-50%)",
-                }} // Toggle the showPassword state
-              >
-                {showPassword ? <Icon icon={eye} /> : <Icon icon={eyeOff} />}
-              </span>
-            </div>
-            <div className="form-group" style={{ position: "relative" }}>
-              <input
-                type={showPassword ? "text" : "password"}
-                className="input"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-                placeholder="Confirm Password"
-              />
-              {confirmPassword !== password && (
-                <div className="error">Passwords do not match</div>
-              )}
-              <span
-                className="toggle-password"
-                onClick={() => setShowPassword(!showPassword)}
-                style={{
-                  position: "absolute",
-                  top: "50%",
-                  right: "10px",
-                  transform: "translateY(-50%)",
-                }} // Toggle the showPassword state
-              >
-                {showPassword ? <Icon icon={eye} /> : <Icon icon={eyeOff} />}
-              </span>
-            </div>
-            <legend>Are you a</legend>
-            <div className="position">
-              <label>
-                <input
-                  type="radio"
-                  name="topping"
-                  value="farmer"
-                  onChange={() => setSelectedRole("farmer")}
-                  checked={selectedRole === "farmer"}
-                />
-                <span>Farmer</span>
-              </label>
-            </div>
-            <div className="position">
-              <label>
-                <input
-                  type="radio"
-                  name="topping"
-                  value="miller"
-                  onChange={() => setSelectedRole("miller")}
-                  checked={selectedRole === "miller"}
-                />
-                <span>Miller</span>
-              </label>
-            </div>
-            <div className="position">
-              <label>
-                <input
-                  type="radio"
-                  name="topping"
-                  value="seller"
-                  onChange={() => setSelectedRole("seller")}
-                  checked={selectedRole === "seller"}
-                />
-                <span>Seller</span>
-              </label>
-            </div>
-            <div className="position">
-              <label>
-                <input
-                  type="radio"
-                  name="topping"
-                  value="Customer"
-                  onChange={() => setSelectedRole("customer")}
-                  checked={selectedRole === "customer"}
-                />
-                <span>Customer</span>
-              </label>
-            </div>
-            <input
-              type="checkbox"
-              checked={check}
-              onChange={() => setCheck(!check)}
-            />{" "}
-            I accept the terms of the offer of privacy policy.
-            <div className="sub but">
-              <Button
-                variant="contained"
-                onClick={navigateToNext}
-                disabled={!isFormValid()}
-              >
-                {" "}
-                SIGN UP
-              </Button>
-            </div>
-          </div>
-        </form>
-        {/* <div class="label-container">
+          </form>
+          {/* <div class="label-container">
           <label>Have an Account? </label>
           <a href="/login">
             <label1>Login Here</label1>
           </a>
         </div> */}
+        </div>
       </div>
+      <Footer/>
     </div>
   );
 };
